e97eaf596b7e1af963b5c6aa539441806de96d61
Den4200 <dennis@dennispham.me>
2020-06-27 23:03:39 -0400

    Add mode function

--- mean-median-mode/src/main.rs
+++ mean-median-mode/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 
 
@@ -29,12 +30,14 @@ fn main() {
     
     println!("The mean is: {}", mean(&numbers));
     println!("The median is: {}", median(&numbers));
+    println!("The mode is: {}", mode(&numbers));
 }
 
 
 fn mean(numbers: &Vec<i32>) -> f32 {
     numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
 }
+
 
 fn median(numbers: &Vec<i32>) -> f32 {
     let len = numbers.len();
@@ -44,4 +47,25 @@ fn median(numbers: &Vec<i32>) -> f32 {
         return numbers[half_len] as f32;
     }
     (numbers[half_len - 1] + numbers[half_len]) as f32 / 2.0
+}
+
+
+fn mode(numbers: &Vec<i32>) -> i32 {
+    let mut num_map: HashMap<i32, u32> = HashMap::new();
+
+    for num in numbers {
+        let amt = num_map.entry(*num).or_insert(0);
+        *amt += 1;
+    }
+
+    let mut max_value = 0;
+    let mut key = 0;
+
+    for (num, amt) in num_map {
+        if amt > max_value {
+            key = num;
+            max_value = amt;
+        }
+    }
+    key
 }
