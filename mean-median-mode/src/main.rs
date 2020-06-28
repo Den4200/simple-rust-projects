23d223af994ffb45ae6e26250866c49c9e62154d
Den4200 <dennis@dennispham.me>
2020-06-27 21:57:55 -0400

    Create mean function

--- mean-median-mode/src/main.rs
+++ mean-median-mode/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input numbers one by one, pressing the Enter key after each one.");
+    println!("Input the letter \"q\" when you want to stop.");
+
+    let mut numbers: Vec<i32> = Vec::new();
+
+    loop {
+        let mut num = String::new();
+
+        io::stdin()
+            .read_line(&mut num)
+            .expect("Failed to read line.");
+
+        num = num.trim().to_string();
+
+        if num == "q" {
+            break;
+        }
+
+        let num: i32 = num
+            .parse()
+            .expect("You did not enter a number.");
+
+        numbers.push(num);
+    }
+    
+    println!("The mean is: {}", mean(&numbers));
+}
+
+
+fn mean(numbers: &Vec<i32>) -> f32 {
+    numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
 }
