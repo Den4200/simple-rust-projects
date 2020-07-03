3b0451ac9b2fc4d5634a3b67b47fe3193514b1df
Den4200 <dennis@dennispham.me>
2020-07-02 23:20:33 -0400

    Add largest package

---
+++ largest/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let numbers = vec![34, 123, 73, 52, 8, 482, 21];
+    println!("The largest number is {}", largest(&numbers));
+}
+
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
