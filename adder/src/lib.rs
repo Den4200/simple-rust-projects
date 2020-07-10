bc3ff183a413592330f05d0ad7891ab4fe27b308
Den4200 <dennis@dennispham.me>
2020-07-09 23:45:41 -0400

    Usage of should_panic in tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -3,6 +3,15 @@ pub fn add_two(x: i32) -> i32 {
 }
 
 
+pub fn add_negatives(x: i32, y: i32) -> i32 {
+    if x >= 0 || y >= 0 {
+        panic!("x and y must be less than 0, not {} and/or {}", x, y);
+    }
+
+    x + y
+}
+
+
 #[cfg(test)]
 mod tests {
     use rand;
@@ -19,5 +28,11 @@ mod tests {
             let num = rand::random::<i32>();
             assert_eq!(num + 2, add_two(num));
         }
+    }
+
+    #[test]
+    #[should_panic(expected = "x and y must be less than 0")]
+    fn test_add_negatives() {
+        add_negatives(-1, 32);
     }
 }
