866e6a5ead84392ba4cbefc58b34785504c5ce1a
Den4200 <dennis@dennispham.me>
2020-07-09 23:32:50 -0400

    Add random value tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -5,10 +5,19 @@ pub fn add_two(x: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
+    use rand;
     use super::*;
 
     #[test]
     fn test_add_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn test_random_add_two() {
+        for _ in 0..3 {
+            let num = rand::random::<i32>();
+            assert_eq!(num + 2, add_two(num));
+        }
     }
 }
