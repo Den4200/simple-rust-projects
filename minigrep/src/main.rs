73ce9191f8e86a87d3262fd8486ae96dc50b150d
Den4200 <dpham.42@hotmail.com>
2020-07-11 02:31:03 -0400

    Add better error handling for Config::new

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
+use std::process;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {} in {}", config.query, config.filename);
 }
@@ -15,10 +20,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments.");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config { query, filename }
+        Ok(Config { query, filename })
     }    
 }
