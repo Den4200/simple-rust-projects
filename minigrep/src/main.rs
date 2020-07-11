c4b03d94aae5f0dd470622a19a61692472a1b042
Den4200 <dpham.42@hotmail.com>
2020-07-11 02:18:48 -0400

    Organize config parsing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,9 +3,21 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {} in {}", config.query, config.filename);
+}
 
-    println!("Searching for {} in {}", query, filename);
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
