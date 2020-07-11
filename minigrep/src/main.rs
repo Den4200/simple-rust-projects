4f3359a0c9401c912873ab11c1ed943ce79528c6
Den4200 <dpham.42@hotmail.com>
2020-07-11 02:21:22 -0400

    Turn parse_config function into new method of Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {} in {}", config.query, config.filename);
 }
@@ -14,10 +14,11 @@ struct Config {
     filename: String
 }
 
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }    
 }
