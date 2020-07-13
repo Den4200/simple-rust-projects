a90b9bc92100949dace54029da79ab98268cab72
Den4200 <dennis@dennispham.me>
2020-07-12 23:19:41 -0400

    Create unit tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,3 +29,31 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_config() {
+        let query = String::from("regex");
+        let filename = String::from("file.txt");
+
+        let config = Config::new(&[String::from("path/to/file"), query.clone(), filename.clone()])
+            .unwrap();
+
+        assert_eq!(query, config.query);
+        assert_eq!(filename, config.filename);
+    }
+
+    #[test]
+    fn test_run() {
+        let config = Config::new(&[String::from("path/to/file"), String::from("file.txt"), String::from("regex")])
+            .unwrap();
+
+        if let Ok(_) = run(config) {
+            panic!("Expected error. Should have failed to read file.");
+        }
+    }
+}
