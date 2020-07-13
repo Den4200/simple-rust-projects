9af8de9d408e29564db33076192211fb0d15c987
Den4200 <dennis@dennispham.me>
2020-07-13 00:33:24 -0400

    Create search_case_insensitive function and its unit tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -35,6 +35,23 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
@@ -75,14 +92,31 @@ mod tests {
     }
 
     #[test]
-    fn test_search() {
+    fn test_search_case_sensitive() {
         let query = "duct";
         let contents = indoc!("\
             Rust
             safe, fast, productive.
             Pick three.
+            Duct tape.
         ");
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn test_search_insensitive() {
+        let query = "rUsT";
+        let contents = indoc!("\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+            Trust me.
+        ");
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
