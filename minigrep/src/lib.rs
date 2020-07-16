0fb9863ad2353ef234f4f7a073b4894c0383aa4a
Den4200 <dennis@dennispham.me>
2020-07-15 23:14:22 -0400

    Use iterator methods for search_case_insensitive function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -58,15 +58,11 @@ pub fn search_case_insensitive<'a>(
     contents: &'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 
