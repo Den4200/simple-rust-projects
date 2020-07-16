a560e64c7a24b27fed04b5be1bf416153128b2ff
Den4200 <dennis@dennispham.me>
2020-07-15 23:11:17 -0400

    Use iterator methods for search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -46,15 +46,10 @@ impl Config {
 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 
